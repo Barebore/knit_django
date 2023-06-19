7e8c0cd27fb7448e3e1951fc7401a6773699ef5d
Борислав Печерских <borislavtarget@gmail.com>
2023-06-19 13:18:30 +0500

    change urlpatterns api_django/

--- knit2/urls.py
+++ knit2/urls.py
@@ -6,10 +6,10 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('pattern_lib/', include('pattern_lib.urls')),
-    path('knowledge_library/', include('knowledge_library.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('admin/', admin.site.urls),
+    path('api_django/pattern_lib/', include('pattern_lib.urls')),
+    path('api_django/knowledge_library/', include('knowledge_library.urls')),
+    path('api_django/ckeditor/', include('ckeditor_uploader.urls')),
+    path('api_django/admin/', admin.site.urls),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
