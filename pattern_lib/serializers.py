084903d134ed59246e12bc1441b092f8a0baa9d7
Борислав Печерских <borislavtarget@gmail.com>
2023-06-22 17:38:26 +0500

    add resolution in view one pattern

--- pattern_lib/serializers.py
+++ pattern_lib/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Pattern, Category
+from PIL import Image
+from django.core.files.storage import default_storage as storage
+
 
 
 # class PatternSerializer(serializers.ModelSerializer):
@@ -22,10 +25,40 @@ class PatternSerializer(serializers.ModelSerializer):
 
 
 class PatternDetailSerializer(serializers.ModelSerializer):
+    pattern_resolution = serializers.SerializerMethodField()
+    scheme_resolution = serializers.SerializerMethodField()
+    scheme_description_resolution = serializers.SerializerMethodField()
+
     class Meta:
         model = Pattern
         fields = '__all__'
 
+    def get_pattern_resolution(self, obj):
+        if obj.pattern:
+            image_path = obj.pattern.path
+            if storage.exists(image_path):
+                with storage.open(image_path, 'rb') as image_file:
+                    image = Image.open(image_file)
+                    return f'{image.width}x{image.height}'
+        return None
+
+    def get_scheme_resolution(self, obj):
+        if obj.scheme:
+            image_path = obj.scheme.path
+            if storage.exists(image_path):
+                with storage.open(image_path, 'rb') as image_file:
+                    image = Image.open(image_file)
+                    return f'{image.width}x{image.height}'
+        return None
+
+    def get_scheme_description_resolution(self, obj):
+        if obj.scheme_description:
+            image_path = obj.scheme_description.path
+            if storage.exists(image_path):
+                with storage.open(image_path, 'rb') as image_file:
+                    image = Image.open(image_file)
+                    return f'{image.width}x{image.height}'
+        return None
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
